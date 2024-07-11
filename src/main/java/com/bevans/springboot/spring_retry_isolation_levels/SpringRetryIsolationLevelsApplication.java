302924687dbb3139b308493101989c042f547b22
Barry Evans <barryevans80@gmail.com>
2024-07-11 13:06:08 +0100

    Spring Boot Starter initial commit

---
+++ src/main/java/com/bevans/springboot/spring_retry_isolation_levels/SpringRetryIsolationLevelsApplication.java
@@ -0,0 +1,13 @@
+package com.bevans.springboot.spring_retry_isolation_levels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryIsolationLevelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRetryIsolationLevelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bevans/springboot/spring_retry_isolation_levels/SpringRetryIsolationLevelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bevans.springboot.spring_retry_isolation_levels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRetryIsolationLevelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
